e46b12596c4777c65708ff32ef83bd625e20c176
IBRAHIM\Ibrahin <ishaheen910@gmail.com>
2023-08-26 17:41:12 +0300

    Update dependencies, remove unused code, update styles

---
+++ app/tickets/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+const Tickets = () => {
+  return (
+    <main>
+      <div className="container">
+        <h1 className="title is-2 has-text-centered">TICKETS</h1>
+      </div>
+    </main>
+  );
+};
+
+export default Tickets;
